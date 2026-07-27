b0e049f23586f7d45013830db96042940b21dd8a
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-07-27 10:32:26 +0700

    feat: enhance login function with error handling and token generation

--- models/controller/userController.js
+++ models/controller/userController.js
@@ -48,10 +48,52 @@ async function login(req, res) {
     try {
         const { email, password } = req.body;
 
-        id ( !email || !password) {
+        if ( !email || !password) {
             return res.status(400).json({
                 message: "Email atau password salah."
             });
         }
+
+        const user = await User.findOne({
+            where: { email }
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Email atau password salah."
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Email atau password salah."
+            });
+        }
+
+        const token = jwt.sign(
+            { 
+                id: user.id, email: user.email 
+            }, 
+                process.env.JWT_SECRET, 
+            { 
+                expiresIn: process.env.JWT_EXPRESS
+
+            });
+
+        return res.status(200).json({
+            message: 'Login berhasil',
+            token
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message
+        });
     }
 }
+
+module.exports = {
+    createUser,
+    login
+};
